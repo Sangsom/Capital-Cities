316509c0f92b3960ff85016a62dbaf31453314a5
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-02 09:27:02 +0300

    Initial Commit

---
+++ Project 16 Capital Cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 16 Capital Cities
+//
+//  Created by Rinalds Domanovs on 02/07/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
